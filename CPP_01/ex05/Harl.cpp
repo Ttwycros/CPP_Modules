895c052b074fb25f59f26e5e840c097a8e53a887
Tregar Twycross <ttwycros@at-q2.msk.21-school.ru>
2022-10-04 18:46:16 +0300

    another one

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -1,35 +1,42 @@
 #include "Harl.hpp"
 
-// HumanA :: HumanA(){}
-// HumanA :: ~HumanA(void){}
+Harl :: Harl()
+{
+	_debub = "I love to get static functions call";
+	_info = "I cannot believe you didn't add static methods!";
+	_warr = "I think I deserve static methods called. I was told it's a necessity!";
+	_err = "This is unacceptable, I will not stand that. I want my static methods now.";
+}
+
+Harl :: ~Harl(void){}
 
 /*static*/ void	Harl :: debug(void)
 {
-	std :: cout << "I love to get static functions call" << std :: endl;
+	std :: cout << _debub << std :: endl;
 }
 
 /*static*/ void	Harl :: info(void)
 {
-	std :: cout << "I cannot believe you didn't add instanse methods!" << std :: endl;
+	std :: cout << _info << std :: endl;
 }
 
 /*static*/ void	Harl :: warning(void)
 { 
-	std :: cout << "I think I deserve instance methods called. I was told it's a necessity!" << std :: endl;
+	std :: cout << _warr << std :: endl;
 }
 
 /*static*/ void	Harl :: error(void)
 {
-	std :: cout << "This is unacceptable, I will not stand that. I want my instance methods now." << std :: endl;
+	std :: cout << _err << std :: endl;
 }
 
 
 /*Yes, i know you can made all of this functions intance methods, but i wanted to try static methods approach.*/
 void	Harl :: complain(std :: string level) {
 	std :: string complainLevels[] = {"debug", "info", "warning", "error"};
-	void	(*forComlain[])(void) = {
-		&debug,
-		&info, //can do either way
+	void	(Harl :: *forComlain[])(void) = {
+		&Harl :: debug,
+		&Harl :: info, //can do either way
 		&Harl :: warning,
 		&Harl :: error
 	};
@@ -41,6 +48,6 @@ void	Harl :: complain(std :: string level) {
 			std :: cerr << "No such level" << std :: endl;
 			break ;
 		default:
-			(*forComlain[i])();
+			(this->*forComlain[i])();
 	}
 }
--- CPP_01/ex05/main.cpp
+++ CPP_01/ex05/main.cpp
@@ -1,15 +1,16 @@
 #include "Harl.hpp"
 
 int main(void) {
+	Harl	karen;
 	std :: cout << "\033[0;33m--DEBUG LEVEL--\033[0;37m" << std :: endl;
-	Harl :: complain("debug");
+	karen.complain("debug");
 	std :: cout << "\033[0;33m--INFO LEVEL---\033[0;37m" << std :: endl;
-	Harl :: complain("info");
+	karen.complain("info");
 	std :: cout << "\033[0;33m-WARNING LEVEL-\033[0;37m" << std :: endl;
-	Harl :: complain("warning");
+	karen.complain("warning");
 	std :: cout << "\033[0;33m--ERROR LEVEL--\033[0;37m" << std :: endl;
-	Harl :: complain("error");
+	karen.complain("error");
 	std :: cout << "\033[0;33mNOT EXISTING LEVEL\033[0;37m" << std :: endl;
-	Harl :: complain("I am not exist");
+	karen.complain("I am not exist");
 	return 0;
 }
