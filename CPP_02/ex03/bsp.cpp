68b388b160ad1146e8d513dc10ed7c3f67ea05c3
Станислав <stanislav@MacBook-Pro-Stanislav.local>
2022-10-15 01:30:10 +0400

    added annotations to functions

--- CPP_02/ex03/bsp.cpp
+++ CPP_02/ex03/bsp.cpp
@@ -1,15 +1,5 @@
 #include "Point.hpp"
 
-// static Fixed	sign(Point p1, Point p2, Point	p3) {
-// 	Fixed	res((p1.getX() - p3.getX()) * (p2.getY() - p3.getY()) - (p1.getY() - p3.getY()) * (p2.getX() - p3.getX()));
-// 	return res;
-// }
-
-// static Fixed	sign(Point point, Point one, Point two)
-// {
-// 	return ((one.getX() - point.getX()) * (two.getY() - one.getY())) - ((two.getX() - one.getX()) * (one.getY() - point.getY()));
-// }
-
 static Fixed	sign(Point const *point, Point const *one, Point const *two)
 {
 	return ((one->getX() - point->getX()) * (two->getY() - one->getY())) - ((two->getX() - one->getX()) * (one->getY() - point->getY()));
@@ -17,12 +7,23 @@ static Fixed	sign(Point const *point, Point const *one, Point const *two)
 
 bool	bsp(Point const A, Point const B, Point const C, Point const point)
 {
+	/*Check for each pair of vertices (for example, A and B),
+	whether the third vertex (for example, C)
+	and the test point (point)
+	lie on the same side of the line
+	passing through this pair of vertices (for example, A and B).*/
+
 	Fixed sign_1, sign_2, sign_3;
 	bool	negative, positive;
 
 	sign_1 = sign(&point, &A, &B);
 	sign_2 = sign(&point, &B, &C);
 	sign_3 = sign(&point, &C, &A);
+	/*If they are of the same sign,
+	then the point is inside the triangle,
+	if one of these is zero,
+	then the point lies on the side,
+	otherwise the point is outside the triangle.*/
 	negative = sign_1 < 0 || sign_2 < 0 || sign_3 < 0;
 	positive = sign_1 > 0 || sign_2 > 0 || sign_3 > 0;
 	return !(negative & positive);
